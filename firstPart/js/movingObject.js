4bcb2a82ba48dff0bca41ba4571a6cf8c3ca17b6
diogofsvilela <developer.dfsv@gmail.com>
2017-10-05 19:49:38 +0100

    Initial development of the movingObject class

---
+++ firstPart/js/movingObject.js
@@ -0,0 +1,87 @@
+class movingObject {
+
+	constructor(initialPosition, initialVelocity, directionOfMovement) {
+		if (!initialPosition instanceof Array) {
+			console.error('Parameter position is not an array');
+			return undefined;
+		}
+
+		if (NaN(initialVelocity)) {
+			console.error('Parameter initialVelocity is not a number');
+		}
+
+		if (!directionOfMovement instanceof Array) {
+			console.error('Parameter directionOfMovement is not an array');
+			return undefined;
+		}
+
+		if (initialPosition.length != directionOfMovement.length) {
+			console.error('initialVelocity and directionOfMovement don\'t have the same dimensions');
+			return undefined;
+		}
+
+		var directionModulus = 0;
+		var directionDimension = directionOfMovement.length;
+
+		for (var i = 0; i < directionDimension; i++) {
+			directionModulus += i * i;
+		}
+
+		directionModulus = Math.sqrt(directionModulus);
+
+		if (directionModulus != 1) {
+			console.error('directionOfMovement doesn\'t represent a unit vector');
+			return undefined;
+		}
+		
+		this.position = initialPosition;
+		this.velocity = initialVelocity;
+		this.directionOfMovement = directionOfMovement;
+	}
+
+	get position() {
+		return this.position;
+	}
+
+	get velocity() {
+		return this.velocity;
+	}
+
+	get directionOfMovement() {
+		return this.directionOfMovement;
+	}
+
+	set position(position) {
+		if (!position instanceof Array) {
+			console.error('Parameter position is not an array');
+		} else {
+			this.position = position;
+		}
+	}
+
+	set velocity(velocity) {
+		if (isNaN(velocity)) {
+			console.error('Parameter velocity is not a number');
+		} else {
+			this.velocity = velocity;
+		}
+	}
+
+	set directionOfMovement(directionOfMovement) {
+		if (!directionOfMovement instanceof Array) {
+			console.error('Parameter directionOfMovement is not an array');
+		} else {
+			this.directionOfMovement = directionOfMovement;
+		}
+	}
+	
+	move() {
+		var dimension = this.directionOfMovement.length;
+
+		for (var i = 0; i < dimension; i++) {
+			this.position[i] += this.directionOfMovement[i] * velocity;
+		}
+
+		return this.position;
+	}
+}
